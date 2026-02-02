763db3c9f644d05df50b5f1210f5c4c6ca4fc1f3
Manoel Lima <manoelsslima@yahoo.com.br>
2026-02-01 21:01:44 -0400

    feat: main and new json item

---
+++ src/main/java/mx/florinda/cardapio/Main.java
@@ -0,0 +1,17 @@
+package mx.florinda.cardapio;
+
+import com.google.gson.Gson;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        ItemCardapio refrescoDoChaves = new ItemCardapio(1L, "Refresco do Chaves", """
+                Suco de limão, que parece Tamarindo mas tem gosto de groselha
+                """, ItemCardapio.CategoriaCardapio.BEBIDAS, new BigDecimal("2.99"), null);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(refrescoDoChaves);
+        System.out.println(json);
+    }
+}
