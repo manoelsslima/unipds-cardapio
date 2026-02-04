eea058b787f9571400e7211dc0e078d1e60afa51
Manoel Lima <manoelsslima@yahoo.com.br>
2026-02-03 23:55:52 -0400

    feat: add findById

--- src/main/java/mx/florinda/cardapio/Database.java
+++ src/main/java/mx/florinda/cardapio/Database.java
@@ -1,44 +1,50 @@
 package mx.florinda.cardapio;
 
 import java.math.BigDecimal;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 
 public class Database {
-    public List<ItemCardapio> listaDeItensCardapio () {
-        List<ItemCardapio> itens = new LinkedList<>();
 
+    private final Map<Long, ItemCardapio> itensPorId = new HashMap<>();
+
+    public Database() {
         ItemCardapio refrescoDoChaves = new ItemCardapio(1L, "Refresco do Chaves", """
                 Suco de limão, que parece Tamarindo mas tem gosto de groselha
                 """, ItemCardapio.CategoriaCardapio.BEBIDAS, new BigDecimal("2.99"), null);
 
-        itens.add(refrescoDoChaves);
+        itensPorId.put(1L, refrescoDoChaves);
 
         ItemCardapio sanduichePresunto = new ItemCardapio(2L, "Sanduíche de Presunto", """
         O clássico sanduíche de presunto do Chaves, simples e saboroso
         """, ItemCardapio.CategoriaCardapio.PRATOS_PRINCIPAIS, new BigDecimal("5.50"), null);
 
-        itens.add(sanduichePresunto);
+        itensPorId.put(2L, sanduichePresunto);
 
         ItemCardapio churrosDaDonaFlorinda = new ItemCardapio(3L, "Churros da Dona Florinda", """
         Churros crocantes recheados com doce de leite cremoso
         """, ItemCardapio.CategoriaCardapio.SOBREMESA, new BigDecimal("4.75"), null);
 
-        itens.add(churrosDaDonaFlorinda);
+        itensPorId.put(3L, churrosDaDonaFlorinda);
 
         ItemCardapio sucoDeTamarindo = new ItemCardapio(4L, "Suco de Tamarindo", """
         Suco de tamarindo bem gelado, especial da Vila
         """, ItemCardapio.CategoriaCardapio.BEBIDAS, new BigDecimal("3.25"), null);
 
-        itens.add(sucoDeTamarindo);
+        itensPorId.put(4L, sucoDeTamarindo);
 
         ItemCardapio tortaDeJamon = new ItemCardapio(5L, "Torta de Jamón", """
         Farta fatia de torta salgada recheada com jamón e queijo
         """, ItemCardapio.CategoriaCardapio.SOBREMESA, new BigDecimal("8.90"), null);
 
-        itens.add(tortaDeJamon);
+        itensPorId.put(5L, tortaDeJamon);
+    }
 
-        return itens;
+    public List<ItemCardapio> listaDeItensCardapio () {
+        return new ArrayList<>(itensPorId.values());
+    }
+
+    public Optional<ItemCardapio> itemCardapioPorId(long id) {
+        return Optional.ofNullable(itensPorId.get(id));
     }
 }
--- src/main/java/mx/florinda/cardapio/Main.java
+++ src/main/java/mx/florinda/cardapio/Main.java
@@ -1,26 +1,16 @@
 package mx.florinda.cardapio;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
 
         Database database = new Database();
         List<ItemCardapio> itens = database.listaDeItensCardapio();
-        for (ItemCardapio item : itens) {
-           System.out.println(item);
-        }
-        System.out.println("------");
-        Set<ItemCardapio.CategoriaCardapio> categorias = new HashSet<>();
-        for  (ItemCardapio item : database.listaDeItensCardapio()) {
-            ItemCardapio.CategoriaCardapio categoria = item.categoria();
-            categorias.add(categoria);
-        }
-
-        categorias.forEach(System.out::println);
-
-        itens.stream().map(ItemCardapio::categoria).forEach(System.out::println);
+        itens.forEach(System.out::println);
+        System.out.println("-----------------");
+        Optional<ItemCardapio> optionalItemCardapio = database.itemCardapioPorId(1L);
+        optionalItemCardapio.ifPresent(itemCardapio -> System.out.println(itemCardapio));
     }
 }
