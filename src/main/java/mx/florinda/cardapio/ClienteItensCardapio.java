3c33762c832c6b5a689ad64e772d6976d486bd5c
Manoel Lima <manoel.lima@manaus.am.gov.br>
2026-02-24 13:19:52 -0400

    feat: add classes for menu item management with HTTP server and JSON generation

---
+++ src/main/java/mx/florinda/cardapio/ClienteItensCardapio.java
@@ -0,0 +1,32 @@
+package mx.florinda.cardapio;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClienteItensCardapio {
+
+    public static void main(String[] args) throws Exception {
+
+        // java 1.0
+        // URL url = new URL("https://viacep.com.br/ws/01001000/json");
+
+//        try (Scanner scanner = new Scanner(url.openStream())) {
+//            while (scanner.hasNext()) {
+//                System.out.println(scanner.nextLine());
+//            }
+//        }
+
+        // java 1.4
+        URI uri = URI.create("http://localhost:8099/itens-cardapio");
+        try (HttpClient httpClient = HttpClient.newHttpClient()) {
+            HttpRequest httpRequest = HttpRequest.newBuilder(uri).build();
+            HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            int statusCode = httpResponse.statusCode();
+            String body = httpResponse.body();
+            System.out.println("Status code: " + statusCode);
+            System.out.println("Body: " + body);
+        }
+    }
+}
---
+++ src/main/java/mx/florinda/cardapio/ServidorItensCardapio.java
@@ -0,0 +1,34 @@
+package mx.florinda.cardapio;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ServidorItensCardapio {
+    public static void main(String[] args) throws IOException {
+
+
+        InetSocketAddress inetSocketAdress = new InetSocketAddress(8099);
+        HttpServer server = HttpServer.create(inetSocketAdress, 0);
+
+        server.createContext("/itensCardapio.json", exchange -> {
+            Path path = Path.of("itensCardapio.json");
+            String json = Files.readString(path);
+            byte[] bytes = json.getBytes();
+
+            Headers headers = exchange.getResponseHeaders();
+            headers.add("Content-Type", "application/json; charset=UTF-8");
+
+            exchange.sendResponseHeaders(200, bytes.length);
+            OutputStream responseBody = exchange.getResponseBody();
+            responseBody.write(bytes);
+        });
+        System.out.println("Subiu servidor http!");
+        server.start();
+    }
+}
