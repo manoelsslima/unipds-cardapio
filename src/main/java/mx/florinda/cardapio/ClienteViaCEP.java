21740499ae9a294e716edf75a13d8146431b898e
Manoel Lima <manoel.lima@manaus.am.gov.br>
2026-02-24 11:15:27 -0400

    feat: simplify URL connection in ClienteViaCEP by using openStream()

--- src/main/java/mx/florinda/cardapio/ClienteViaCEP.java
+++ src/main/java/mx/florinda/cardapio/ClienteViaCEP.java
@@ -1,19 +1,14 @@
 package mx.florinda.cardapio;
 
-import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.Scanner;
 
 public class ClienteViaCEP {
 
     public static void main(String[] args) throws Exception {
         URL url = new URL("https://viacep.com.br/ws/01001000/json");
-        // abre a conexão
-        URLConnection urlConnection = url.openConnection();
 
-        // pega a inputstream
-        Scanner scanner = new Scanner(urlConnection.getInputStream());
+        Scanner scanner = new Scanner(url.openStream());
         while(scanner.hasNext()) {
             System.out.println(scanner.nextLine());
         }
