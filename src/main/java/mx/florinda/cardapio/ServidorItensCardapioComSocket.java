2f6c0efb76bb754a0fb1b41f391b42c873c1752a
Manoel Lima <manoelsslima@yahoo.com.br>
2026-03-02 00:37:49 -0400

    feat: add threading support to socket server for handling requests concurrently

--- src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
+++ src/main/java/mx/florinda/cardapio/ServidorItensCardapioComSocket.java
@@ -16,32 +16,42 @@ public class ServidorItensCardapioComSocket {
             System.out.println("Servidor de itens do cardapio rodando na porta 8099...");
 
             while (true) {
-                try (Socket clientSocket = serverSocket.accept()) {
-                    InputStream clientIS = clientSocket.getInputStream();
-
-                    StringBuilder requestBuilder = new StringBuilder();
-                    int data;
-                    do {
-                        data = clientIS.read();
-                        requestBuilder.append((char) data);
-                    } while (clientIS.available() > 0);
-
-                    String request = requestBuilder.toString();
-                    System.out.println(request); // out é um PrintStream
-
-                    Path path = Path.of("itensCardapio.json");
-                    String json = Files.readString(path);
-
-                    OutputStream clientOS = clientSocket.getOutputStream();
-                    PrintStream clientOut = new PrintStream(clientOS); // crio o PrintStream para enviar para o out
-
-                    clientOut.println("HTTP/1.1 200 OK");
-                    clientOut.println("Content-Type: application/json; charset=UTF-8");
-                    clientOut.println("Content-Length: " + json.getBytes().length);
-                    clientOut.println(); // linha em branco para separar os headers do body
-                    clientOut.println(json);
-                }
+                Socket clientSocket = serverSocket.accept();
+                Thread thread = new Thread(() -> trataRequisicao(clientSocket));
+                thread.start();
             }
+        }
+    }
+
+    private static void trataRequisicao(Socket clientSocket) {
+        try (clientSocket){ // try-with-resources para garantir que o socket seja fechado após o tratamento da requisição
+            InputStream clientIS = clientSocket.getInputStream();
+
+            StringBuilder requestBuilder = new StringBuilder();
+            int data;
+            do {
+                data = clientIS.read();
+                requestBuilder.append((char) data);
+            } while (clientIS.available() > 0);
+
+            String request = requestBuilder.toString();
+            System.out.println(request); // out é um PrintStream
+
+            Thread.sleep(250); // simula um processamento mais demorado
+
+            Path path = Path.of("itensCardapio.json");
+            String json = Files.readString(path);
+
+            OutputStream clientOS = clientSocket.getOutputStream();
+            PrintStream clientOut = new PrintStream(clientOS); // crio o PrintStream para enviar para o out
+
+            clientOut.println("HTTP/1.1 200 OK");
+            clientOut.println("Content-Type: application/json; charset=UTF-8");
+            clientOut.println("Content-Length: " + json.getBytes().length);
+            clientOut.println(); // linha em branco para separar os headers do body
+            clientOut.println(json);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
